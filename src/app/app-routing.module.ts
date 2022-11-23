e3c58ebdf8beb4e6c33c3e17f103a90d6a69cde6
priyanka-sasikumar123 <priyankasasikumar567@gmail.com>
2022-11-23 12:03:07 +0530

    Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:LoginComponent
+  },
+  //dashboard 4200/dashboard
+  {
+    path:'dashboard',component:DashboardComponent
+  },
+  //register 4200/register
+  {
+    path:'register',component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,16 +15,35 @@ export class LoginComponent implements OnInit { //(3rd executed)
   pswd='';
   
   userDetails:any={
-    1000:{accountno:1000,username:'arathi',password:1000},
-    1001:{accountno:1001,username:'amritha',password:1001},
-    1002:{accountno:1002,username:'pallavi',password:1002}
+    1001:{acno:1001,username:'arathi',password:1001},
+    1002:{acno:1002,username:'amritha',password:1002},
+    1003:{acno:1003,username:'pallavi',password:1003}
   }
+  login(){
+    // alert("login clicked")    //Event Binding example
+     var acno=this.acno;
+     var pswd=this.pswd;
+     var userDetails=this.userDetails;
+     if(acno in userDetails){
+       if(pswd == userDetails[acno]['password'])
+       {
+        alert("Login successfully")
+       }
+       else{
+        alert('Incorrect password')
+       }
+     }
+     else{
+      alert('Invalid AccountNo')
+     }
+    }
   
   constructor(){ //(first execuetd)
  //it automatically invoked when the object is created
  //object initialization
  
 }
+
 acnochange(event:any){
   console.log(event);
   this.acno=event.target.value; //input value come here
@@ -37,30 +56,29 @@ pswdchange(event:any){
   
 }
 
+
 ngOnInit():void{ //(second executed)
   //its a life cycle hooks of angular
   //when the component is created at same time it will intialize  or authorize
 }
-// 
 
-login(a:any,p:any){
-  // alert("login clicked")    //Event Binding example
-   var accno=a.value;
-   var psd=p.value;
-   var user_dt=this.userDetails;
+// login(a:any,p:any){
+//   // alert("login clicked")    //Event Binding example
+//    var accno=a.value;
+//    var psd=p.value;
+//    var user_dt=this.userDetails;
 
-   if(accno in user_dt){
-     if(psd == user_dt[accno].password){
-      alert("Login successfully")
-     }
-     else{
-      alert('Incorrect password')
-     }
-   }
-   else{
-    alert('Invalid AccountNo')
-   }
-}
-
+//    if(accno in user_dt){
+//      if(psd == user_dt[accno].password){
+//       alert("Login successfully")
+//      }
+//      else{
+//       alert('Incorrect password')
+//      }
+//    }
+//    else{
+//     alert('Invalid AccountNo')
+//    }
+// }
 
 }
